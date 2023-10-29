ffd2e429b20f524c7237eab847ec9c844e9d2eb2
i.kłusek <J6ZkqwVUn>
2023-10-29 22:01:01 +0100

    TD-01-Api_connection_innit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: console.log(data.todo);
+	$: todo = data.todo;
+
+	let todoString = '';
+
+	onMount(() => {
+		todoString = JSON.stringify(todo);
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{todoString}</p>
