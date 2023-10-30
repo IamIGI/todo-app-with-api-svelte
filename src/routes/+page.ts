39c2fde4f602604d35f0a7815e8678eb619fd3a3
i.kłusek <J6ZkqwVUn>
2023-10-30 10:10:22 +0100

    TD-02-Project stucture

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,8 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const todoList = await fetch('api/todo');
+	const todoList = await fetch('/api/todo');
 	const todoJSON = await todoList.json();
-	console.log(todoJSON);
 	return {
 		todo: todoJSON
 	};
